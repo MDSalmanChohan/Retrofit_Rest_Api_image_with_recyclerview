919f079660ae988cd565dfed88822afac17678d4
MD Salman <108273905+MDSalmanChohan@users.noreply.github.com>
2023-10-16 16:09:44 +0530

    Create DataModel.java

---
+++ DataModel.java
@@ -0,0 +1,17 @@
+public class DataModel {
+    private String title;
+    private String description;
+    private String imageUrl;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
